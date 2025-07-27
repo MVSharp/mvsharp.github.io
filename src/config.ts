6015d2157daa46b6237b7f53766ba243986315e1
MVSharp <zxcnio93@gmail.com>
2025-07-27 19:03:10 +0800

    update:seo config
    update: for seo

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-  title: "MVSharp",
-  subtitle: "C#",
+  title: "MVSharp - C# & Rust Programming Insights",
+  subtitle: "Sharing CSharp & IT Knowledges With My Personal Experiences",
   lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 225, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
   avatar: "assets/images/images.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: "MVSharp",
-  bio: "CSharp Lover",
+  bio: "Hi I am Michael VanHouHei , I am a CSharp Lover ,current learning some rust , fall in love with sofie keyboard",
   links: [
     // {
     // 	name: "Twitter",
